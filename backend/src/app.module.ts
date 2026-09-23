4f41dd505de13e539f27ccfecb4bf442fe9a2941
Vasylyna <chornei.vasylyna@gmail.com>
2026-09-23 18:06:39 +0300

    chore(backend): setup postgres docker container and base drizzle structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			expandVariables: true,
+		}),
+	],
+	controllers: [],
+	providers: [],
 })
-
 export class AppModule {}
